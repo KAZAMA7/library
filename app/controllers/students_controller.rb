58c840a06df3731e3d477e81a928f9f9fb5c1610
Kazama7 <giridhar0241@gmail.com>
2018-02-04 13:45:27 +0530

    added a new definition new in stuents controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,19 @@
 class StudentsController < ApplicationController
 	def new
 	end
+
+	def create
+
+	    @student = Student.new(params[:student])
+	    @student.save
+	    redirect_to @student
+	
+	end
+	
+	private
+
+    def student_params
+        params.require(:student).permit(:first_name, :last_name, :date_of_birth, :phone, :address)
+    end
+
 end
