99de22118ff896f2474bbfef9fcf9c04705471e5
Kazama7 <giridhar0241@gmail.com>
2018-02-05 17:52:21 +0530

    application is working upto index

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,19 +1,22 @@
 class StudentsController < ApplicationController
+	def index
+		@students = Student.all
+	end
+
 	def new
 	end
 
 	def create
-
-	    @student = Student.new(params[:student])
+	    @student = Student.new(student_params)
 	    @student.save
 	    redirect_to @student
-	
+	end
+	def show
+		@student = Student.find(params[:id])
 	end
 	
 	private
-
-    def student_params
-        params.require(:student).permit(:first_name, :last_name, :date_of_birth, :phone, :address)
+	def student_params
+      params.require("student").permit(:first_name, :last_name, :dob,  :phone, :address)
     end
-
 end
